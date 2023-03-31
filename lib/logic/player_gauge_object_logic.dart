aaca6f6ffc661c915e2f288a69e725bc732e19cd
aprxty3 <ajiprxty3@gmail.com>
2023-04-01 01:10:58 +0700

    config gauge logic

--- lib/logic/player_gauge_object_logic.dart
+++ lib/logic/player_gauge_object_logic.dart
@@ -1,3 +1,45 @@
-import 'package:flame/components.dart';
+import 'dart:async';
+import 'dart:ui';
 
-class PlayerGauge extends PositionComponent {}
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+import 'player_object_logic.dart';
+
+class PlayerGauge extends PositionComponent {
+  double _healthLeft = 1.0;
+
+  @override
+  FutureOr<void> onLoad() {
+    final playerParent = parent;
+    if (playerParent is Player) {
+      width = playerParent.width;
+      height = 10;
+      anchor = Anchor.centerLeft;
+      position = Vector2(0, 0);
+    }
+    return super.onLoad();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    canvas.drawRect(
+        Rect.fromPoints(
+          const Offset(0, 0),
+          Offset(width, height),
+        ),
+        Paint()..color = Colors.white);
+    canvas.drawRect(
+        Rect.fromPoints(
+          const Offset(0, 0),
+          Offset(width * _healthLeft, height),
+        ),
+        Paint()
+          ..color = _healthLeft > 0.5
+              ? Colors.green
+              : _healthLeft > 0.25
+                  ? Colors.orange
+                  : Colors.red);
+  }
+}
