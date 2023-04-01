23d13df0cec61f51fde3de51aab63bf113c7508f
aprxty3 <ajiprxty3@gmail.com>
2023-04-01 23:34:23 +0700

    connecting to supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_game_with_flutter_flame/view/game_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  await Supabase.initialize(
+    url: 'https://bwbdrvvkkugnitjejevx.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3YmRydnZra3Vnbml0amVqZXZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODAzNjQxNzMsImV4cCI6MTk5NTk0MDE3M30.jxaJ94PCt0QnaGZ4f9E1iJcyhb9U1oG_-XM7zsH1XhI',
+    realtimeClientOptions: const RealtimeClientOptions(eventsPerSecond: 40),
+  );
+  runApp(const MyApp());
+}
+
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
