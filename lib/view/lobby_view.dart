9532dd54db0461a441c0b0568bd2e94caa4b7649
aprxty3 <ajiprxty3@gmail.com>
2023-04-01 23:41:00 +0700

    handle lobby wait to other player

--- lib/view/lobby_view.dart
+++ lib/view/lobby_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:uuid/uuid.dart';
+
+import '../main.dart';
 
 class LobbyView extends StatefulWidget {
   const LobbyView({
@@ -12,10 +16,55 @@ class LobbyView extends StatefulWidget {
 }
 
 class _LobbyViewState extends State<LobbyView> {
-  final List<String> _userids = [];
+  List<String> _userids = [];
   bool _loading = false;
 
-  final myUserId = '';
+  final myUserId = const Uuid().v4();
+
+  late final RealtimeChannel _lobbyChannel;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _lobbyChannel = supabase.channel(
+      'lobby',
+      opts: const RealtimeChannelConfig(self: true),
+    );
+    _lobbyChannel.on(RealtimeListenTypes.presence, ChannelFilter(event: 'sync'),
+        (payload, [ref]) {
+      // Update the lobby count
+      final presenceState = _lobbyChannel.presenceState();
+
+      setState(() {
+        _userids = presenceState.values
+            .map((presences) =>
+                (presences.first as Presence).payload['user_id'] as String)
+            .toList();
+      });
+    }).on(RealtimeListenTypes.broadcast, ChannelFilter(event: 'game_start'),
+        (payload, [_]) {
+      // Start the game if someone has started a game with you
+      final participantIds = List<String>.from(payload['participants']);
+      if (participantIds.contains(myUserId)) {
+        final gameId = payload['game_id'] as String;
+        widget.onGameStarted(gameId);
+        Navigator.of(context).pop();
+      }
+    }).subscribe(
+      (status, [ref]) async {
+        if (status == 'SUBSCRIBED') {
+          await _lobbyChannel.track({'user_id': myUserId});
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    supabase.removeChannel(_lobbyChannel);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,9 +81,27 @@ class _LobbyViewState extends State<LobbyView> {
           onPressed: _userids.length < 2
               ? null
               : () async {
-                  setState(() {
-                    _loading = true;
-                  });
+                  setState(
+                    () {
+                      _loading = true;
+                    },
+                  );
+
+                  final opponentId =
+                      _userids.firstWhere((userId) => userId != myUserId);
+                  final gameId = const Uuid().v4();
+
+                  await _lobbyChannel.send(
+                    type: RealtimeListenTypes.broadcast,
+                    event: 'game_start',
+                    payload: {
+                      'participants': [
+                        opponentId,
+                        myUserId,
+                      ],
+                      'game_id': gameId,
+                    },
+                  );
                 },
           child: const Text('start'),
         ),
